bf165c0adcaf80d98cfb7ead8b55134fb2694ab3
amit <amit4njoy@gmail.com>
2020-02-16 20:18:42 +0530

    Spring cloud stream rabbitmq consumer

---
+++ SpringCloudStreamWithRabbitMq/spring-cloud-stream-consumer/src/main/java/com/avtech/springcloud/springcloudstreamconsumer/SpringCloudStreamConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.avtech.springcloud.springcloudstreamconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamConsumerApplication.class, args);
+	}
+
+}
