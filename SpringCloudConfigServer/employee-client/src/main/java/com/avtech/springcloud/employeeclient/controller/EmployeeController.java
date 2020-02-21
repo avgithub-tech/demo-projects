374508ca8eb2f50d83c0d7113ff9970c62ab8ef9
amit <amit4njoy@gmail.com>
2020-02-21 22:16:55 +0530

    Spring cloud config server

--- SpringCloudConfigServer/employee-client/src/main/java/com/avtech/springcloud/employeeclient/controller/EmployeeController.java
+++ SpringCloudConfigServer/employee-client/src/main/java/com/avtech/springcloud/employeeclient/controller/EmployeeController.java
@@ -4,24 +4,32 @@ package com.avtech.springcloud.employeeclient.controller;
 import com.avtech.springcloud.employeeclient.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/employee-service")
 public class EmployeeController {
 
     @Value("${employee.service.url}")
     private String url;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private RestTemplate template;
 
     @GetMapping("/get-employees")
     public List<Employee> getAllEmployees(){
-        List<Employee> employees = restTemplate.getForObject(url,List.class);
+        List<Employee> employees = template.getForObject(url,List.class);
         return employees;
+    }
+
+    @Bean
+    public RestTemplate template(){
+        return new RestTemplate();
     }
 }
